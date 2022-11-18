74db468b0ea1d54b62b48d5d314b980e9dc5594c
OLIVER TAYLOR <olivertaylor1295@hotmail.com>
2022-11-18 18:26:26 +0000

    end of day need to work on function (symbol)

--- javaScript.js
+++ javaScript.js
@@ -1,54 +1,72 @@
 // ALL THE BUTTONS ON THE CALCULATOR DEFINED
-
 const cSymbol = document.querySelector(".c");
-const percentageSymbol = document.querySelector(".percentage");
-const divideSymbol = document.querySelector(".divide");
-const oneSymbol = document.querySelector(".sec--1");
-const twoSymbol = document.querySelector(".sec--2");
-const threeSymbol = document.querySelector(".sec--3");
-const timesSymbol = document.querySelector(".times");
-const fourSymbol = document.querySelector(".sec--4");
-const fiveSymbol = document.querySelector(".sec--5");
-const sixSymbol = document.querySelector(".sec--6");
-const minusSymbol = document.querySelector(".minus");
-const sevenSymbol = document.querySelector(".sec--7");
-const eightSymbol = document.querySelector(".sec--8");
-const nineSymbol = document.querySelector(".sec--9");
-const plusSymbol = document.querySelector(".plus");
-const zeroSymbol = document.querySelector(".sec--0");
-const fullstopSymbol = document.querySelector(".full-stop");
-const equalSymbol = document.querySelector(".equal");
 const inputbox = document.querySelector(".input");
-
-
-// HEADER DEFINED
-const h1 = document.querySelector(".body--h1");
+const equalSymbol = document.querySelector(".equal");
 
 
 
 
-// GET INPUT TO DISPLAY ZERO
-    cSymbol.addEventListener("click", () => {
-        h1.innerHTML = 0;
-    });
+// arrays for buttons and function buttons
+const buttons = Array.from(document.querySelectorAll(".num-button"));
+const functionButton = Array.from(
+  document.querySelectorAll(".function-button")
+);
 
-// ADDS ONE INTO H1 TAG
-    oneSymbol.addEventListener("click", () => {
-        let oneSymbol = 1;
-       h1.innerHTML = h1.innerHTML + oneSymbol;
-    });
 
-// ADDS TWO INTO H1 TAG
-    twoSymbol.addEventListener("click", () => {
-        let twoSymbol = 2;
-        h1.innerHTML = h1.innerHTML + twoSymbol;
-    });
 
-// EQUALS BOTH THE ONE AND TWO 
-    equalSymbol.addEventListener("click", () => {
-        
-        let equalSymbol = oneSymbol + twoSymbol
-        h1.innerHTML = h1.innerHTML + equalSymbol
-    })
 
-   
+//element for displaying the output of my functions
+let display = document.querySelector(".total-display");
+
+let number = "";
+let symbol = "";
+
+//This input the numbers into display
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    return (display.innerHTML += button.innerHTML);
+  });
+});
+
+//function and click events to return the add/take/divide/times of a sum
+
+functionButton.forEach((fButton) => {
+  fButton.addEventListener("click", () => {
+    symbol += fButton.innerHTML;
+    return (display.innerHTML += fButton.innerHTML);
+  });
+});
+
+
+// Takes the 0 away from the display when clicked
+cSymbol.addEventListener("click", () => {
+  display.innerHTML = "";
+});
+
+
+//function button that uses equals
+equalSymbol.addEventListener("click", () => {
+  if (symbol === "%") {
+    alert("symbol has been set correctly")
+    display.innerText = number;
+  } else if (symbol === "+") {
+    number = number + Number(display.innerHTML);
+    display.innerHTML = answer;
+  }
+});
+
+
+
+
+
+
+
+
+////function and click event to show the current value as a percentage
+
+//function and click event to clear the output and reset number value to 0
+
+//function and click event to make numbers negative
+
+//equals button will perform all of the tasks that the symbols require using if statements.
---
+++ junk.js
@@ -0,0 +1,53 @@
+
+// const oneSymbol = document.querySelector(".sec--1");
+// const twoSymbol = document.querySelector(".sec--2");
+// const threeSymbol = document.querySelector(".sec--3");
+// const fourSymbol = document.querySelector(".sec--4");
+// const fiveSymbol = document.querySelector(".sec--5");
+// const sixSymbol = document.querySelector(".sec--6");
+// const sevenSymbol = document.querySelector(".sec--7");
+// const eightSymbol = document.querySelector(".sec--8");
+// const nineSymbol = document.querySelector(".sec--9");
+// const zeroSymbol = document.querySelector(".sec--0");
+
+
+// const cSymbol = document.querySelector(".c");
+// const percentageSymbol = document.querySelector(".percentage");
+// const divideSymbol = document.querySelector(".divide");
+// const timesSymbol = document.querySelector(".times");
+// const minusSymbol = document.querySelector(".minus");
+// const plusSymbol = document.querySelector(".plus");
+// const fullstopSymbol = document.querySelector(".full-stop");
+// const equalSymbol = document.querySelector(".equal");
+
+
+
+
+
+
+
+
+
+// // GET INPUT TO DISPLAY ZERO
+// cSymbol.addEventListener("click", () => {
+//   headerOne.innerHTML = 0;
+// });
+
+// // ADDS ONE INTO H1 TAG
+// oneSymbol.addEventListener("click", () => {
+//   let oneSymbol = 1;
+//   headerOne.innerHTML = headerOne.innerHTML + oneSymbol;
+// });
+
+// // ADDS TWO INTO H1 TAG
+// twoSymbol.addEventListener("click", () => {
+//   let twoSymbol = 2;
+//   headerOne.innerHTML = headerOne.innerHTML + twoSymbol;
+// });
+
+// // ITERATES OF H1 ADDS NUMBER TO VARIABLE TO DISPLAY TO H1
+
+// equalSymbol.addEventListener("click", () => {
+//   newH1 = h1.split();
+//   console.log(newH1);
+// });
