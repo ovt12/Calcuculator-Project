d2428bed68b9f7b05d21fe5d8835eaec3c98ab91
OLIVER TAYLOR <olivertaylor1295@hotmail.com>
2022-11-20 21:15:28 +0000

    functions working unable to display to displayOne

--- javaScript.js
+++ javaScript.js
@@ -26,55 +26,47 @@ numbuttons.forEach((button) => {
 });
 
 //function and click events to return the add/take/divide/times of a sum
-const totalSum = (e) => {
+const calculationTotal = (e) => {
   number = Number(displayOne.innerHTML);
   symbol = e.target.innerHTML;
-  console.log(symbol);
-  displayOne.innerHTML = ""
+  displayOne.innerHTML = "";
   console.log(number)
-
+  console.log(symbol)
 };
-plusSymbol = addEventListener("click", totalSum);
 
-
-
-
-
-
-
+divideSymbol = addEventListener("click", calculationTotal);
+timesSymbol = addEventListener("click", calculationTotal);
+minusSymbol = addEventListener("click", calculationTotal);
+plusSymbol = addEventListener("click", calculationTotal);
 
 // Takes the 0 away from the display when clicked
 
-const reset = (e) => {
-displayOne.innerHTML = ""
-}
-cSymbol.addEventListener("click", (reset))
-
-
-
-
-
-
+const reset = (event) => {
+  displayOne.innerHTML = "";
+};
+cSymbol.addEventListener("click", reset);
 
 // function button that uses all the function buttons
 
 const equals = (event) => {
-    if (symbol === "+") {
-        number += Number(displayOne.innerHTML)
-        displayOne.innerHTML = number
-    } 
-}
+  if (symbol === "+") {
+    number += Number(displayOne.innerHTML);
+    displayOne.innerHTML = number;
+  } else if (symbol === "-") {
+    number -= Number(displayOne.innerHTML);
+    displayOne.innerHTML = number;
+  } else if (symbol === "x") {
+    number *= Number(displayOne.innerHTML);
+    displayOne.innerHTML = number;
+  } else if (number === "/") {
+    number /= Number(displayOne.innerHTML);
+    displayOne.innerHTML = number;
+  }
+};
 
-equalSymbol.addEventListener("click", equals)
-
-
-
-//function to store result in first
+equalSymbol.addEventListener("click", equals);
 
 ////function and click event to show the current value as a percentage
 
-//function and click event to clear the output and reset number value to 0
-
 //function and click event to make numbers negative
 
-//equals button will perform all of the tasks that the symbols require using if statements.
